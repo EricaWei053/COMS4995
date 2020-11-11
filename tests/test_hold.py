41f6ffc95d1cd75134bcc833f80589342f7b0c76
Chenchen(Erica) Wei <32824389+EricaWei053@users.noreply.github.com>
2020-11-11 11:15:00 -0500

    Add plot file, 5 test holder.

--- tests/test_hold.py
+++ tests/test_hold.py
@@ -1,7 +1,16 @@
-
+import os.path
 def test_1():
     assert 0 == 0
 
-
 def test_2():
     assert 1 != 2
+
+def test_3():
+    assert os.path.exists("../data/STRG1.csv")
+
+def test_4():
+    assert os.path.exists("../data/STRG2.csv")
+
+def test_5():
+    assert os.path.exists("../src/plot.py")
+
